cd012ed55eed1209ce79522311b38199b38c7c8d
Saad <mehmoodsaad347@gmail.com>
2026-01-26 02:00:31 +0530

    added

--- src/config/GenerateToken.ts
+++ src/config/GenerateToken.ts
@@ -13,7 +13,7 @@ const generateToken = async (userId: string, role: string, res: Response) => {
     httpOnly: true,
     sameSite: "none",
     maxAge: 10 * 24 * 60 * 60 * 1000,
-    secure: process.env.NODE_ENV === "production",
+    // secure: process.env.NODE_ENV === "production",
     path: "/",
   });
   return token;
